dfd7f783fdcceff46d5f77f9ae7fd1f1de1b0d51
unknown <shohidullah.ru@outlook.com>
2023-09-07 05:49:32 +0600

    Lesson 10 completed, xss clean and express rate limit package introduced for api security.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Ecommerce_Ali_Express
+
+1. 
+2. Environment Setup
+3. Create Express server --> express
+4. HTTP request & response
+5. nodemon and morgan package --> nodemon, morgan
+6. API testing with Postman
+7. Midleware & Types of Midleware
+8. Express Error Handling Midleware --> body-parser
+9. How to handle HTTP errors --> http-errors
+10. How to secure API --> xss-clean, express-rate-limit
--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
 const createError = require('http-errors');
+const xssClean = require('xss-clean');
+const rateLimit = require('express-rate-limit')
 const app = express();
 
+const rateLimiter = rateLimit({
+    windowMs: 1*60*1000, // 1 minute
+    max:5,
+    message: 'To many request from this IP. please try a later.'
+})
+
 // Midlewares
+app.use(xssClean());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 
-app.get('/test', (req, res ) => { 
+app.get('/test', rateLimiter, (req, res ) => { 
     res.status(200).send({
         message: "Welcome to the server"
     })
