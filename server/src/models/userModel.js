adb86e23a9c69f8c9141f23780e421e2ced99af8
unknown <shohidullah.ru@outlook.com>
2023-09-15 23:51:26 +0600

    Schema and Model for User, Bcrypt package introduction

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,12 +1,58 @@
+const {Schema, model} = require('mongoose');
+const bcrypt = require('bcrypt');
+const { defaultImagePath } = require('../secret');
 
 
-const users = [
-    {id:1, name: 'shohidullah', age:33},
-    {id:2, name: 'Hamid', age:31},
-    {id:3, name: 'Sana', age:20}
- ]
+ const userSchema = new Schema({
+   name: {
+      type: String,
+      required: [true, "Username is required"],
+      trim:true,
+      minlength:[3, 'Username atleast 3 character'],
+      maxlength:[31, 'Username is maximum 31 character']
+   },
+   email: {
+      type: String,
+      required: [true, "User email is required"],
+      trim:true,
+      unique:true,
+      lowercase:true,
+      validate: {
+         validator: (v)=> {
+             return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+         },
+         message:"Please enter a valid email"
+     }
+   },
+   password: {
+      type: String,
+      required: [true, "Password is required"],
+      trim:true,
+      minlength:[3, 'Password atleast 8 character'],
+      set: (v)=> bcrypt.hashSync(v, bcrypt.genSaltSync(10))
+   },
+   image: {
+      type: String,
+      default: defaultImagePath
+   },
+   address: {
+      type: String,
+      required: [true, "Address is required"],
+   },
+   phone: {
+      type: String,
+      required: [true, "Address is required"],
+   },
+   isAdmin: {
+      type: Boolean,
+      default:false
+   },
+   isBanned: {
+      type: Boolean,
+      default:false
+   },
+ }, {timestamps:true});
 
+ const User = model('Users', userSchema)
 
- module.exports ={
-    users
- }
+ module.exports = User
--- server/src/secret.js
+++ server/src/secret.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 const serverPort = process.env.PORT || 3000; // Default to port 3000 if PORT is not set in .env
 
 const mongoDBURL = process.env.MONGODB_ATLAS_URL|| "mongodb://localhost:27017/ecommerceDB"
-
+const defaultImagePath = process.env.DEFAULT_USER_IMAGE_PATH || 'public/images/users/default.png'
 module.exports = {
     serverPort,
-    mongoDBURL
+    mongoDBURL,
+    defaultImagePath
 };
